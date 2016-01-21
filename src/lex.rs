d9f7f50ad9ee2db066f52f78f05c6cbd5a942e04
w338 <w338trash@yahoo.com>
2016-01-20 19:44:18 -0800

    Rewrite lexer to handle weird token combinations.

--- src/lex.rs
+++ src/lex.rs
@@ -15,145 +15,164 @@ pub enum Token {
 
 pub struct Lexer<'a> {
     source: &'a str,
-    lead_index: usize,
-    lead_char: char,
     chars: ::std::str::CharIndices<'a>,
+    reversed: Vec<(usize, char)>,
     table: &'a mut symbol::Table
 }
 
 impl<'a> Lexer<'a> {
     fn new(source: &'a str, table: &'a mut symbol::Table) -> Self {
-        let mut chars = source.char_indices();
-        if let Some((index, char)) = chars.next() {
-            Lexer {
-                source: source,
-                lead_index: index,
-                lead_char: char,
-                chars: chars,
-                table: table
-            }
-        } else {
-            Lexer {
-                source: source,
-                lead_index: 0,
-                lead_char: '\0',
-                chars: chars,
-                table: table
-            }
+        Lexer {
+            source: source,
+            chars: source.char_indices(),
+            reversed: Vec::new(),
+            table: table
         }
     }
 }
 
 impl<'a> Lexer<'a> {
-    fn step(&mut self, start_index: usize, end_index: usize, lead_char: char) -> &'a str {
-        self.lead_index = end_index;
-        self.lead_char = lead_char;
+    fn tick(&mut self) -> (usize, char) {
+        if let Some(res) = self.reversed.pop() {
+            res
+        } else {
+            match self.chars.next() {
+                Some(res) => res,
+                None => (self.source.len(), '\0')
+            }
+        }
+    }
+
+    fn untick(&mut self, index: usize, c: char) {
+        self.reversed.push((index, c));
+    }
+
+    fn slice(&mut self, start_index: usize, end_index: usize) -> &'a str {
         &self.source[start_index..end_index]
     }
 
     fn intern(&mut self, source: &str) -> symbol::Symbol {
         self.table.intern(source)
     }
+
+    fn slice_intern(&mut self, start_index: usize, end_index: usize) -> symbol::Symbol {
+        let s = self.slice(start_index, end_index);
+        self.intern(s)
+    }
 }
 
 impl<'a> Iterator for Lexer<'a> {
     type Item = Token;
     fn next(&mut self) -> Option<Token> {
-        let start_index = self.lead_index;
-        let mut s = &self.source[start_index..];
-        if self.lead_char == '\0' {
-            return None;
-        } else if UnicodeXID::is_xid_start(self.lead_char) {
+        let (first_index, first_char) =  self.tick();
+        if UnicodeXID::is_xid_start(first_char) {
             // We have an identifier.
-            while let Some((index, char)) = self.chars.next() {
+            loop {
+                let (index, char) = self.tick();
                 if !UnicodeXID::is_xid_continue(char) {
-                    s = self.step(start_index, index, char);
-                    break;
+                    return Some(Token::Identifier(self.slice_intern(first_index, index)));
                 }
             }
-            return Some(Token::Identifier(self.intern(s)));
-        } else if self.lead_char.is_whitespace() {
+        } else if first_char.is_whitespace() {
             // We have some whitespace.
-            while let Some((index, char)) = self.chars.next() {
+            loop {
+                let (index, char) = self.tick();
                 if !char.is_whitespace() {
-                    s = self.step(start_index, index, char);
-                    break;
+                    return Some(Token::Whitespace(self.slice_intern(first_index, index)));
                 }
             }
-            return Some(Token::Whitespace(self.intern(s)));
-        } else if let Some(digit) = self.lead_char.to_digit(10) {
+        } else if let Some(digit) = first_char.to_digit(10) {
             // We have a number.
-            if let Some((index, char)) = self.chars.next() {
-                if char.is_digit(10) || char == '_' || char == '.' || char == 'e' || char == 'E' {
-                    // Decimal (integer of floating point).
-                    let mut iter = ::std::iter::once((index, char)).chain(s.char_indices());
-                    let mut saw_decimal = false;
-                    let mut saw_exponent = false;
-                    while let Some((index, char)) = iter.next() {
-                        if !char.is_digit(10) {
-                            if char == '.' &&
-                               !saw_decimal &&
-                               s[(index + 1)..].starts_with(|c: char| c.is_digit(10) ||
-                                                                                c == 'e' || 
-                                                                                c == 'E') {
-                                saw_decimal = true;
-                            } else if char == 'e' &&
-                                      !saw_exponent &&
-                                      s[(index + 1)..].starts_with(|c: char| c.is_digit(10)) {
-                                saw_exponent = true;
-                            } else {
-                                s = self.step(start_index, index, char);
-                                break;
-                            }
-                        }
+            let (second_index, second_char) = self.tick();
+            if second_char.is_digit(10) ||
+               second_char == '_' ||
+               second_char == '.' ||
+               second_char == 'e' ||
+               second_char == 'E' {
+                // Decimal (integer or floating point).
+                self.untick(second_index, second_char);
+                let mut last_index;
+                loop {
+                    let (index, char) = self.tick();
+                    if !char.is_digit(10) {
+                        self.untick(index, char);
+                        last_index = index;
+                        break;
                     }
-                    // TODO(w338): Implement number tags.
-                    if saw_exponent || saw_decimal {
-                        return Some(Token::Number(val::Number::F64(s.parse().unwrap())));
-                    } else {
-                        return Some(Token::Number(val::Number::I64(s.parse().unwrap())));
+                }
+
+                let mut saw_decimal = false;
+
+                let (index, char) = self.tick();
+                if char == '.' {
+                    saw_decimal = true;
+                    loop {
+                        let (index, char) = self.tick();
+                        if !char.is_digit(10) {
+                            self.untick(index, char);
+                            last_index = index;
+                            break;
+                        }
                     }
                 } else {
-                    let start_index = index + 1;
-                    let mut s = &self.source[start_index..];
-                    if digit == 0 && char == 'x' {
-                        while let Some((index, char)) = self.chars.next() {
-                            if !char.is_digit(16) {
-                                s = self.step(start_index, index, char);
+                    self.untick(index, char);
+                }
+
+                let mut saw_exponent = false;
+
+                let (index, char) = self.tick();
+                if char == 'e' || char == 'E' {
+                    let (index, char) = self.tick();
+                    if char.is_digit(10) {
+                        saw_exponent = true;
+                        loop {
+                            let (index, char) = self.tick();
+                            if !char.is_digit(10) {
+                                self.untick(index, char);
+                                last_index = index;
                                 break;
                             }
                         }
-                        println!("s = {:?}", s);
-                        return Some(Token::Number(val::Number::U64(u64::from_str_radix(s, 16).unwrap())));
-                    } else if digit == 0 && char == 'o' {
-                        while let Some((index, char)) = self.chars.next() {
-                            if !char.is_digit(8) {
-                                s = self.step(start_index, index, char);
-                                break;
-                            }
-                        }
-                        return Some(Token::Number(val::Number::U64(u64::from_str_radix(s, 8).unwrap())));
-                    } else if digit == 0 && char == 'b' {
-                        while let Some((index, char)) = self.chars.next() {
-                            if !char.is_digit(2) {
-                                s = self.step(start_index, index, char);
-                                break;
-                            }
-                        }
-                        return Some(Token::Number(val::Number::U64(u64::from_str_radix(s, 2).unwrap())));
                     } else {
-                        self.lead_index = index;
-                        self.lead_char = char;
+                        self.untick(index, char);
                     }
+                }
+                if !saw_exponent {
+                    self.untick(index, char);
+                }
+                let s = self.slice(first_index, last_index);
+                // TODO(w338): Implement number tags.
+                if saw_exponent || saw_decimal {
+                    return Some(Token::Number(val::Number::F64(s.parse().unwrap())));
+                } else {
+                    return Some(Token::Number(val::Number::I64(s.parse().unwrap())));
+                }
+            }
+            if digit == 0 {
+                if let Some(radix) = match second_char {
+                        'x' => Some(16),
+                        'o' => Some(8),
+                        'b' => Some(2),
+                        _ => None
+                    } {
+                    let (third_index, third_char) = self.tick();
+                    if third_char.is_digit(radix) {
+                        loop {
+                            let (index, char) = self.tick();
+                            if !char.is_digit(radix) {
+                                let s = self.slice(third_index, index);
+                                return Some(Token::Number(val::Number::U64(u64::from_str_radix(s, radix).unwrap())));
+                            }
+                        }
+                    }
+                    self.untick(third_index, third_char);
+                    self.untick(second_index, second_char);
                 }
             }
             return Some(Token::Number(val::Number::I64(digit as i64)));
         } else {
-            // We must have an operator.
-            //let start_index = self.lead_index;
-            panic!("what {:?}", self.lead_char);
+            return None;
         }
-        unimplemented!()
     }
 }
 
@@ -194,4 +213,59 @@ fn it_lexes_hexadecimals() {
     let mut tab = symbol::Table::new();
     assert_eq!(Lexer::new("0x1", &mut tab).next(), Some(Token::Number(val::Number::U64(1))));
     assert_eq!(Lexer::new("0x1 ", &mut tab).next(), Some(Token::Number(val::Number::U64(1))));
+}
+
+#[test]
+fn it_lexes_octals() {
+    let mut tab = symbol::Table::new();
+    assert_eq!(Lexer::new("0o1", &mut tab).next(), Some(Token::Number(val::Number::U64(1))));
+    assert_eq!(Lexer::new("0o1 ", &mut tab).next(), Some(Token::Number(val::Number::U64(1))));
+}
+
+#[test]
+fn it_lexes_binary() {
+    let mut tab = symbol::Table::new();
+    assert_eq!(Lexer::new("0b1", &mut tab).next(), Some(Token::Number(val::Number::U64(1))));
+    assert_eq!(Lexer::new("0b1 ", &mut tab).next(), Some(Token::Number(val::Number::U64(1))));
+}
+
+#[test]
+fn it_lexes_weird_combinations() {
+    let mut tab = symbol::Table::new();
+    let e = tab.intern("e");
+    let ever = tab.intern("ever");
+    let x = tab.intern("x");
+    let o = tab.intern("o");
+    let b = tab.intern("b");
+    let big = tab.intern("big");
+    {
+        let mut lexer = Lexer::new("1e", &mut tab);
+        assert_eq!(lexer.next(), Some(Token::Number(val::Number::I64(1))));
+        assert_eq!(lexer.next(), Some(Token::Identifier(e)));
+    }
+    {
+        let mut lexer = Lexer::new("1ever", &mut tab);
+        assert_eq!(lexer.next(), Some(Token::Number(val::Number::I64(1))));
+        assert_eq!(lexer.next(), Some(Token::Identifier(ever)));
+    }
+    {
+        let mut lexer = Lexer::new("0x", &mut tab);
+        assert_eq!(lexer.next(), Some(Token::Number(val::Number::I64(0))));
+        assert_eq!(lexer.next(), Some(Token::Identifier(x)));
+    }
+    {
+        let mut lexer = Lexer::new("0o", &mut tab);
+        assert_eq!(lexer.next(), Some(Token::Number(val::Number::I64(0))));
+        assert_eq!(lexer.next(), Some(Token::Identifier(o)));
+    }
+    {
+        let mut lexer = Lexer::new("0b", &mut tab);
+        assert_eq!(lexer.next(), Some(Token::Number(val::Number::I64(0))));
+        assert_eq!(lexer.next(), Some(Token::Identifier(b)));
+    }
+    {
+        let mut lexer = Lexer::new("0big", &mut tab);
+        assert_eq!(lexer.next(), Some(Token::Number(val::Number::I64(0))));
+        assert_eq!(lexer.next(), Some(Token::Identifier(big)));
+    }
 }
